7c73d19e58b1d7b0dc47c8d62c60b1ad49fb8a0a
Rajesh Nagireddy <rajesh.rnagireddy@gmail.com>
2018-05-23 07:48:21 +0530

    added draft

--- examples/index.js
+++ examples/index.js
@@ -1,2 +1,11 @@
+// import calendar from 'calendar';
+
 document.getElementById('app').innerHTML = 'hello';
 document.getElementById('loader').remove();
+
+
+// const cal = new calendar.Calendar();
+// const m = cal.monthDays(2018, 4);
+// for (let i=0; i<m.length; i++) {
+//   console.log(m[i]);
+// }
